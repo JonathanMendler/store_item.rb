83396c178b0042d4f32329e5dcb7fc140dbaff85
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-02 14:12:09 -0500

    Created 3 products as hashes

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+product_01 = {"color" = green, "price" = 12, "inventory" = 36}
+product_02 = {"color" = orange, "price" = 14, "inventory" = 7}
+product_03 = {"color" = red, "price" = 28, "inventory" = 11}
