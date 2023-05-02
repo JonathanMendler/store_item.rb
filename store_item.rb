7e6fa124f2cce7c009ec5bf8f90a937733ff0ac4
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-02 15:51:37 -0500

    Converted lines 1-5 to notes. Modified line 7. Modified class method.

--- store_item.rb
+++ store_item.rb
@@ -1,10 +1,10 @@
-product_01 = { :color => "green", :price => 12, :inventory => 36 }
-product_02 = { :color => "orange", :price => 14, :inventory => 7 }
-product_03 = { :color => "red", :price => 28, :inventory => 11 }
+#product_01 = { :color => "green", :price => 12, :inventory => 36 }
+#product_02 = { :color => "orange", :price => 14, :inventory => 7 }
+#product_03 = { :color => "red", :price => 28, :inventory => 11 }
 
-puts "#{product_01[:color]}"
+#puts "#{product_01[:color]}"
 
-Class.Product
+class Product
 
   def initialize(color, price, inventory)
     @color = color
@@ -23,7 +23,12 @@ Class.Product
   def inventory
     @inventory
   end
+
+  def print_info
+    puts "The item is #{color} and is selling for #{price}. We have #{inventory} in stock."
+  end
 end
 
-product_01.new=("green", 12, 36)
-product_02.new=("orange", 14, 7)
+product_01 = Product.new ("green", 12, 36)
+product_02 = Product.new ("orange", 14, 7)
+product_03 = Product.new ("red", 28, 11)
