d4d2f568393ba3f2b57bcf234579da811e51bf0b
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-02 15:41:21 -0500

    Added Class to code

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,26 @@
 product_01 = { :color => "green", :price => 12, :inventory => 36 }
 product_02 = { :color => "orange", :price => 14, :inventory => 7 }
 product_03 = { :color => "red", :price => 28, :inventory => 11 }
+
+puts "#{product_01[:color]}"
+
+Class.Product
+
+  def initialize(color, price, inventory)
+    @color = color
+    @price = price
+    @inventory = inventory
+  end
+  
+  def color
+    @color
+  end
+
+  def price
+    @price
+  end
+
+  def inventory
+    @inventory
+  end
+end
