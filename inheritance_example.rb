0f224f121c685c2b2581ffab6150c2927681c1a6
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-03 13:03:28 -0500

    Added class Food & definitions

---
+++ inheritance_example.rb
@@ -0,0 +1,55 @@
+class Vehicle
+  def initialize(input_options)
+    @color = input_options[color:]
+    @make = input_options[make:]
+    @model = input_options[model:]
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car < Vehicle
+  def drive_train
+    @drive_train = input_options[drive_train:]  
+  end
+  
+  def fuel_type
+    @fuel_type = input_options[fuel_type:] 
+  end
+  
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Vehicle
+
+  def frame_material
+    @frame_material = input_options[frame_material:]
+  end
+
+  def weight
+    @weight = input_options[weight:]
+  end
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+car_01 = Car.new(color: "silver", make: "Honda", model: "Civic", drive_train: "2WD")
+bike_01 = Bike.new(speed: 14, direction: east)
+puts car_01
--- store_item.rb
+++ store_item.rb
@@ -35,9 +35,20 @@ class Product
   end
 end
 
+class Food < Product
+  def initialize(input_options)
+    super
+    @shelf_life = input_options[shelf_life:]
+  end
+
+  def food_info
+    puts "The item is #{color} and is selling for #{price}. We have #{inventory} in stock. Item needs to be sold or removed from display within #{shelf_life}"
+end
+
 product_01 = Product.new(color: "green", price: 12, inventory: 36)
 product_02 = Product.new(color: "orange", price: 14, inventory: 7)
 product_03 = Product.new(color: "red", price: 28, inventory: 11)
+food_product_01 = Food.new(color: "yellow", price: 6, inventory: 27, shelf_life: "6 days")
 puts product_01.color
 product_02.print_info
 puts product_01.add_tax
