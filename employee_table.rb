10af2067daf0355f32859c1426efc96e7d7a05ed
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-03 15:43:28 -0500

    Added methods (Create, Read, Update, Delete)

--- employee_table.rb
+++ employee_table.rb
@@ -1,14 +1,31 @@
-# class Employee
-#   attr_reader :first_name, :last_name, :salary, :active
-#   attr_writer :active
+class Employee
+  attr_reader :first_name, :last_name, :salary, :active
+  attr_writer :active
 
-#   def initialize(input_options)
-#     @first_name = input_options[:first_name]
-#     @last_name = input_options[:last_name]
-#     @salary = input_options[:salary]
-#     @active = input_options[:active]
-#   end
-# end
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
+  end
+
+  def create
+  end
+
+  def read
+  end
+
+  def updadte
+  end
+
+  def delete
+  end
+    
+
+end
+
+
+
 
 # table = TTY::["first_name", "last_name", "salary", "active"]
 table = TTY::Table.new(["first_name", "last_name", "salary", "active"][["Majora", "Carter", 80000, true]
