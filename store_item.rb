88ccbe9d6f8d5a515e5fd8222047de5b43145a0d
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-02 22:45:04 -0500

    Added writer at line 26. Removed space after new on 35-37. Added puts on 38, print_info on 39.

--- store_item.rb
+++ store_item.rb
@@ -5,13 +5,12 @@
 #puts "#{product_01[:color]}"
 
 class Product
-
   def initialize(color, price, inventory)
     @color = color
     @price = price
     @inventory = inventory
   end
-  
+
   def color
     @color
   end
@@ -24,11 +23,17 @@ class Product
     @inventory
   end
 
+  def add_tax
+    @price = 1.06 * @price
+  end
+
   def print_info
     puts "The item is #{color} and is selling for #{price}. We have #{inventory} in stock."
   end
 end
 
-product_01 = Product.new ("green", 12, 36)
-product_02 = Product.new ("orange", 14, 7)
-product_03 = Product.new ("red", 28, 11)
+product_01 = Product.new("green", 12, 36)
+product_02 = Product.new("orange", 14, 7)
+product_03 = Product.new("red", 28, 11)
+puts product_01.color
+product_02.print_info
