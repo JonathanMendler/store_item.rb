ef923877e80b419502603b9f7681ff0ec274c502
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-04 11:56:15 -0500

    Added requiremnts for store_item file.

--- store_item.rb
+++ store_item.rb
@@ -1,8 +1,5 @@
-#product_01 = { :color => "green", :price => 12, :inventory => 36 }
-#product_02 = { :color => "orange", :price => 14, :inventory => 7 }
-#product_03 = { :color => "red", :price => 28, :inventory => 11 }
-
-#puts "#{product_01[:color]}"
+require "./Food_product.rb"
+require "./store_module.rb"
 
 class Product
   attr_reader :color, :price, :inventory
