7861294e9feb231c573ac671b92aa0daba836190
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-05 12:46:19 -0500

    Added examples & pronounciation.

--- dictionary.rb
+++ dictionary.rb
@@ -1,5 +1,7 @@
 require "http"
 
+APIKEY = "zoykch8rrpuv19tuv0o2igg7ketsgrcz213sank54les4zjfv"
+
 system "clear"
 puts "Do you require definitions? I'm here to help!"
 print "What is your word? "
@@ -10,4 +12,6 @@ response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?li
 dictionary = response.parse(:json)
 
 definition = dictionary[0]["text"]
+example = dictionary[0]["exampleUses"]
+pronounce = dictionary[0]["textProns"]
 puts "The definition of #{word} is #{definition}"
