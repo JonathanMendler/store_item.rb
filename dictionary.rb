1d8011e97bb6ff6dbdeaee39a68bcc5f68605b44
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-05 12:41:30 -0500

    Completed code with user input. Removed API Key.

--- dictionary.rb
+++ dictionary.rb
@@ -2,10 +2,12 @@ require "http"
 
 system "clear"
 puts "Do you require definitions? I'm here to help!"
+print "What is your word? "
+word = gets.chomp
 
-response = HTTP.get("https://api.wordnik.com/v4/word.json/ghoul/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{APIKEY}")
+response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{APIKEY}")
 
 dictionary = response.parse(:json)
 
 definition = dictionary[0]["text"]
-puts "The definition of ghoul is #{definition}"
+puts "The definition of #{word} is #{definition}"
