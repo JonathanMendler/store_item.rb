4908b15946bcb14f00b99413fab526c35a5ea09e
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-05 13:12:07 -0500

    Attempting a while loop.

--- dictionary.rb
+++ dictionary.rb
@@ -1,17 +1,24 @@
 require "http"
 
-APIKEY = "zoykch8rrpuv19tuv0o2igg7ketsgrcz213sank54les4zjfv"
+APIKEY = ""
 
 system "clear"
 puts "Do you require definitions? I'm here to help!"
-print "What is your word? "
-word = gets.chomp
+while true
+  print "What is your word? "
+  word = gets.chomp
 
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{APIKEY}")
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{APIKEY}")
 
-dictionary = response.parse(:json)
+  dictionary = response.parse(:json)
 
-definition = dictionary[0]["text"]
-example = dictionary[0]["exampleUses"]
-pronounce = dictionary[0]["textProns"]
-puts "The definition of #{word} is #{definition}"
+  definition = dictionary[0]["text"]
+  example = dictionary[0]["exampleUses"]
+  pronounce = dictionary[0]["textProns"]
+  puts "You've chosen the word #{word}, which is pronounced #{pronounce}. Its definition is '#{definition}' and an example of it in use would be #{example}."
+  puts "Would you like to try another word? Type 'q' to quit."
+  if word == "q"
+    puts "Goodbye!"
+    break
+  end
+end
